ebdcc518e346ec8c807cb2c5d556f6e385cc8548
reksarka <reksarka@gmail.com>
2022-02-06 20:27:12 +0200

    More clarity.

--- iget.sh
+++ iget.sh
@@ -34,21 +34,44 @@ fi
 
 url_embed="${url}embed/"
 
-curl $url_embed --compressed -s \
-  -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" \
-  -H "accept-encoding: gzip, deflate, br" \
-  -H "cache-control: no-cache" \
-  -H "pragma: no-cache" \
-  -H "dnt: 1" \
-  -H "sec-ch-ua: \"Chromium\";v=\"94\", \"Google Chrome\";v=\"94\", \";Not A Brand\";v=\"99\"" \
-  -H "sec-ch-ua-mobile: ?0" \
-  -H "sec-ch-ua-platform: \"Windows\"" \
-  -H "sec-fetch-dest: document" \
-  -H "sec-fetch-mode: navigate" \
-  -H "sec-fetch-site: none" \
-  -H "sec-fetch-user: ?1" \
-  -H "upgrade-insecure-requests: 1" \
-  -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36" \
-| grep EmbeddedMediaImage \
-| sed 's/\&amp;/\&/g' \
-| sed -r 's/.*src="(.*)" srcset.*/\1/'
+# Headers for user agent faking.
+
+headers='-H "accept: text/html,application/xhtml+xml,application/xml;'
+headers+='q=0.9,image/avif,image/webp,image/apng,*/*;'
+headers+='q=0.8,application/signed-exchange;v=b3;q=0.9"'
+
+headers+=' -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+headers+=' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71'
+headers+=' Safari/537.36"'
+
+headers+=' -H "sec-ch-ua: \"Chromium\";v=\"94\", \"Google Chrome\";'
+headers+='v=\"94\", \";Not A Brand\";v=\"99\""'
+
+headers+=' -H "accept-encoding: gzip, deflate, br"'
+headers+=' -H "cache-control: no-cache"'
+headers+=' -H "dnt: 1"'
+headers+=' -H "pragma: no-cache"'
+headers+=' -H "sec-ch-ua-mobile: ?0"'
+headers+=' -H "sec-ch-ua-platform: \"Windows\""'
+headers+=' -H "sec-fetch-dest: document"'
+headers+=' -H "sec-fetch-mode: navigate"'
+headers+=' -H "sec-fetch-site: none"'
+headers+=' -H "sec-fetch-user: ?1"'
+headers+=' -H "upgrade-insecure-requests: 1"'
+
+# Instagram response will be gzipped, so we need the `--compressed`.
+get_embed="curl $url_embed $headers --silent --compressed"
+
+# Expect that response will contain the `img.<img_class>`, that looks like:
+#   <img class="<img_class>" alt="..." src="<url-1080>"
+#     srcset="<url-640> 640w,<url-740> 740w,<url-1080> 1080w" />
+img_class=EmbeddedMediaImage
+find_img_str="grep $img_class"
+
+# &amp; -> &
+convert_amp="sed 's/\&amp;/\&/g'"
+
+# Cut the `1080w` image URL from the `img.srcset`.
+cut_url="sed -r 's/.*srcset=\".*(https:[^:]*) 1080w.*/\\1/'"
+
+eval "$get_embed | $find_img_str | $convert_amp | $cut_url"
