6b8ee9703397d07a811d46826e44ab60578af16d
reksarka <reksarka@gmail.com>
2022-02-23 23:40:46 +0200

    Splits the shell script onto functions.

--- iget.sh
+++ iget.sh
@@ -32,79 +32,112 @@
 #   oh=00_AT9ZCu6t-GoPDRFMmMZOIUgpqYk8-lYKm8vPpkb0FZUCGg&oe=621D0264&
 #   _nc_sid=78c662
 
-url=$1
-given_width=$2
+exit_if_invalid_post_url()
+{
+  id_length=11
+  id_regexp="[A-Za-z0-9+-_@]{$id_length}"
+  url_regexp="https://www.instagram.com/p/$id_regexp/?$"
 
-id_length=11
-id_regex="[A-Za-z0-9+-_@]{$id_length}"
-url_regex="https://www.instagram.com/p/$id_regex/?$"
+  if ! [[ $1 =~ $url_regexp ]]
+  then
+    echo Bad URL. Required https://www.instagram.com/p/\<post_id\>/
+    exit 1
+  fi
+}
 
-if ! [[ $url =~ $url_regex ]]
-then
-  echo Bad URL. Required https://www.instagram.com/p/\<post_id\>/
-  exit 1
-fi
+validate_image_width()
+{
+  DEFAULT_WIDTH=1080
+  AVAILABLE_WIDTH=($DEFAULT_WIDTH 750 640)
 
-# Add a slash if needed.
-if [[ "${url: -1}" != "/" ]]
-then
-  url="$url/"
-fi
+  local given_width=${1:-$DEFAULT_WIDTH}
+  local valid_width=$DEFAULT_WIDTH
 
-# Spoofing a request with embedded Instagram post query.
-embed_query='cr=1\&v=14\&wp=540\&rd=%2Fstatic%2Fimage.html'
-# Convert post URL -> URL for post embedding.
-url_embed="${url}embed/?${embed_query}"
+  for available_width in ${AVAILABLE_WIDTH[*]}
+  do
+    if [ $available_width == $given_width ]
+    then
+      valid_width=$given_width
+      break
+    fi
+  done
 
-# HTTP headers for user agent spoofing.
+  echo $valid_width
+}
 
-headers='-H "accept: text/html,application/xhtml+xml,application/xml;'
-headers+='q=0.9,image/avif,image/webp,image/apng,*/*;'
-headers+='q=0.8,application/signed-exchange;v=b3;q=0.9"'
+embedded_post_url()
+{
+  local url=$1
 
-headers+=' -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-headers+=' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71'
-headers+=' Safari/537.36"'
+  # Add a slash if needed.
+  if [[ "${url: -1}" != "/" ]]
+  then
+    url="$url/"
+  fi
 
-headers+=' -H "sec-ch-ua: \"Chromium\";v=\"94\", \"Google Chrome\";'
-headers+='v=\"94\", \";Not A Brand\";v=\"99\""'
+  # Spoofing a request with embedded Instagram post query.
+  embed_query='cr=1\&v=14\&wp=540\&rd=%2Fstatic%2Fimage.html'
 
-headers+=' -H "accept-encoding: gzip, deflate, br"'
-headers+=' -H "cache-control: no-cache"'
-headers+=' -H "dnt: 1"'
-headers+=' -H "pragma: no-cache"'
-headers+=' -H "sec-ch-ua-mobile: ?0"'
-headers+=' -H "sec-ch-ua-platform: \"Windows\""'
-headers+=' -H "sec-fetch-dest: document"'
-headers+=' -H "sec-fetch-mode: navigate"'
-headers+=' -H "sec-fetch-site: none"'
-headers+=' -H "sec-fetch-user: ?1"'
-headers+=' -H "upgrade-insecure-requests: 1"'
-
-# Response will be gzipped, so we need `--compressed`.
-get_embed="curl $url_embed $headers --silent --compressed"
-
-# An embedded Instagram post is expected to contain an img like:
-#   <img class="<img_class>" alt="..." src="<url-1080>"
-#     srcset="<url-640> 640w,<url-750> 750w,<url-1080> 1080w" />
-img_class=EmbeddedMediaImage
-find_img_str="grep $img_class"
+  # Convert post URL -> URL for post embedding.
+  echo "${url}embed/?${embed_query}"
+}
 
 # &amp; -> &
-convert_amp="sed 's/\&amp;/\&/g'"
+convert_amp()
+{
+  sed 's/\&amp;/\&/g' <<< "$1"
+}
 
-DEFAULT_WIDTH=1080
-VALID_WIDTH=($DEFAULT_WIDTH 750 640)
-given_width=${given_width:-$DEFAULT_WIDTH}
-for w in ${VALID_WIDTH[*]}
-do
-  if [ $w == $given_width ]
-  then
-    width=$given_width
-    break
-  fi
-done
-width=${width:-$DEFAULT_WIDTH}
-cut_url="sed -r 's/.*srcset=\".*(https:[^:]*) ${width}w.*/\\1/'"
+http_get()
+{
+  # HTTP headers for user agent spoofing.
 
-eval "$get_embed | $find_img_str | $convert_amp | $cut_url"
+  headers='-H "accept: text/html,application/xhtml+xml,application/xml;'
+  headers+='q=0.9,image/avif,image/webp,image/apng,*/*;'
+  headers+='q=0.8,application/signed-exchange;v=b3;q=0.9"'
+
+  headers+=' -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+  headers+=' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71'
+  headers+=' Safari/537.36"'
+
+  headers+=' -H "sec-ch-ua: \"Chromium\";v=\"94\", \"Google Chrome\";'
+  headers+='v=\"94\", \";Not A Brand\";v=\"99\""'
+
+  headers+=' -H "accept-encoding: gzip, deflate, br"'
+  headers+=' -H "cache-control: no-cache"'
+  headers+=' -H "dnt: 1"'
+  headers+=' -H "pragma: no-cache"'
+  headers+=' -H "sec-ch-ua-mobile: ?0"'
+  headers+=' -H "sec-ch-ua-platform: \"Windows\""'
+  headers+=' -H "sec-fetch-dest: document"'
+  headers+=' -H "sec-fetch-mode: navigate"'
+  headers+=' -H "sec-fetch-site: none"'
+  headers+=' -H "sec-fetch-user: ?1"'
+  headers+=' -H "upgrade-insecure-requests: 1"'
+
+  # Response will be gzipped, so we need `--compressed`.
+  curl $1 $headers --silent --compressed
+}
+
+filter_image_url()
+{
+  local html=$1
+  local width=$(validate_image_width $2)
+
+  # An embedded Instagram post is expected to contain an img like:
+  #   <img class="<img_class>" alt="..." src="<url-1080>"
+  #     srcset="<url-640> 640w,<url-750> 750w,<url-1080> 1080w" />
+  local img_class=EmbeddedMediaImage
+  local img_str=$(grep $img_class <<< "$html")
+  img_str=$(convert_amp "$img_str")
+  sed -r "s/.*srcset=\".*(https:[^:]*) ${width}w.*/\\1/" <<< "$img_str"
+}
+
+post_url=$1
+image_width=$2
+
+exit_if_invalid_post_url $post_url
+
+embedded_url=$(embedded_post_url $post_url)
+embedded_post_html=$(http_get $embedded_url)
+filter_image_url "$embedded_post_html" $image_width
