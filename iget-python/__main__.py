9d3567f05b26c2d5f46176181351180eed31615e
reksarka <reksarka@gmail.com>
2022-05-28 12:54:24 +0300

    Adds help for Python CLI args.

--- iget-python/__main__.py
+++ iget-python/__main__.py
@@ -1,11 +1,6 @@
-from args import parse_args
+from args import args
 from iget import img_url
 
 
-def main():
-    args = parse_args()
-    print(img_url(args.url, args.width))
-
-
 if __name__ == '__main__':
-    main()
+    print(img_url(*args()))
--- iget-python/args.py
+++ iget-python/args.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser, Action, ArgumentError
+from argparse import ArgumentParser, ArgumentError, Action
 from iget import DEFAULT_WIDTH, AVAILABLE_WIDTH, is_post_url
 
 
@@ -7,34 +7,40 @@ class ValidatePostUrl(Action):
     def __call__(self, parser, namespace, value, options=''):
 
         if is_post_url(value):
-            namespace.url = value
+            namespace.post_url = value
         else:
-            raise ArgumentError(self, 'ERROR: Invalid URL of Instagram post!')
+            raise ArgumentError(self, 'Invalid URL of Instagram post!')
 
 
-class ValidateWidth(Action):
+class ValidateImgWidth(Action):
 
     def __call__(self, parser, namespace, value, options=''):
 
         if value in AVAILABLE_WIDTH:
-            namespace.width = value
+            namespace.img_width = value
         else:
-            namespace.width = DEFAULT_WIDTH
+            namespace.img_width = DEFAULT_WIDTH
             print('WARNING: Invalid width! The default value is used.')
 
 
-def parse_args():
+def args():
     parser = ArgumentParser()
+
     parser.add_argument(
-        'url',
+        'post_url',
         type=str,
         action=ValidatePostUrl,
+        help='https://www.instagram.com/p/<ID>',
     )
+
     parser.add_argument(
-        'width',
+        'img_width',
         type=int,
         nargs='?',
         default=DEFAULT_WIDTH,
-        action=ValidateWidth,
+        action=ValidateImgWidth,
+        help='Post image width: [1080, 750, 640] px',
     )
-    return parser.parse_args()
+
+    args = parser.parse_args()
+    return args.post_url, args.img_width
